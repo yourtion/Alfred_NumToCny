b3506fa2542c855005766e3e165b181cba7afd60
YourtionGuo <yourtion@gmail.com>
2016-02-27 11:47:43 +0800

    更新算法 https://gist.github.com/oldrev/6638736

---
+++ Source/NumToCny.py
@@ -0,0 +1,97 @@
+#encoding: utf-8
+'''
+中文互联网上迄今为止实现最正确、代码最漂亮且效率最高的大写人民币金额转换代码
+作者：李维 <oldrev@gmail.com>
+版权所有 (c) 2013 李维。保留所有权利。
+本代码基于 BSD License 授权。
+'''
+
+from cStringIO import StringIO
+import math
+
+_RMB_DIGITS = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖' ]
+_SECTION_CHARS = ['', '拾', '佰', '仟', '万' ]
+
+def to_rmb_upper(price):
+    price = round(price, 2)
+    integer_part = int(price)
+    wanyi_part = integer_part / 1000000000000
+    yi_part = integer_part % 1000000000000 / 100000000
+    wan_part = integer_part % 100000000 / 10000
+    qian_part = integer_part % 10000
+    dec_part = int(price * 100 % 100)
+
+    strio = StringIO()
+
+    zero_count = 0
+    #处理万亿以上的部分
+    if integer_part >= 1000000000000 and wanyi_part > 0:
+        zero_count = _parse_integer(strio, wanyi_part, zero_count, True)
+        strio.write('万')
+
+    #处理亿到千亿的部分
+    if integer_part >= 100000000 and yi_part > 0:
+        is_first_section = integer_part >= 100000000 and integer_part < 1000000000000 
+        zero_count = _parse_integer(strio, yi_part, zero_count, is_first_section)
+        strio.write('亿')
+
+    #处理万的部分
+    if integer_part >= 10000 and wan_part > 0:
+        is_first_section = integer_part >= 1000 and integer_part < 10000000 
+        zero_count = _parse_integer(strio, wan_part, zero_count, is_first_section)
+        strio.write('万')
+
+    #处理千及以后的部分
+    if qian_part > 0:
+        is_first_section = integer_part < 1000
+        zero_count = _parse_integer(strio, qian_part, zero_count, is_first_section)
+    else:
+        zero_count += 1
+    if integer_part > 0:
+        strio.write('圆')
+
+    #处理小数
+    if dec_part > 0: 
+        _parse_decimal(strio, integer_part, dec_part, zero_count)
+    elif dec_part == 0 and integer_part > 0:
+        strio.write('整')
+    else:
+        strio.write('零圆整')
+
+    return strio.getvalue()
+
+def _parse_integer(strio, value, zero_count = 0, is_first_section = False):
+    assert value > 0 and value <= 9999
+    ndigits = int(math.floor(math.log10(value))) + 1
+    if value < 1000 and not is_first_section:
+        zero_count += 1
+    for i in xrange(0, ndigits):
+        factor = int(pow(10, ndigits - 1 - i))
+        digit = int(value / factor)
+        if digit != 0:
+            if zero_count > 0:
+                strio.write('零')
+            strio.write(_RMB_DIGITS[digit])
+            strio.write(_SECTION_CHARS[ndigits - i - 1])
+            zero_count = 0
+        else:
+            zero_count += 1
+        value -= value / factor * factor
+    return zero_count
+
+def _parse_decimal(strio, integer_part, value, zero_count):
+    assert value > 0 and value <= 99
+    jiao = value / 10
+    fen = value % 10
+    if zero_count > 0 and (jiao > 0 or fen > 0) and integer_part > 0:
+        strio.write('零')
+    if jiao > 0:
+        strio.write(_RMB_DIGITS[jiao])
+        strio.write('角')
+    if zero_count == 0 and jiao == 0 and fen > 0 and integer_part > 0:
+        strio.write('零')
+    if fen > 0:
+        strio.write(_RMB_DIGITS[fen])
+        strio.write('分')
+    else:
+        strio.write('整') 
--- Source/source.py
+++ Source/source.py
@@ -1,39 +1,6 @@
-# -*- coding: utf-8 -*-
+#encoding: utf-8
 
-import math     
-def numtoCny(num):     
-	capUnit = ['万','亿','万','圆','']     
-	capDigit = { 2:['角','分',''], 4:['仟','佰','拾','']}     
-	capNum=['零','壹','贰','叁','肆','伍','陆','柒','捌','玖']     
-	snum = str('%019.02f') % num     
-	if snum.index('.')>16:     
-		return '输入数据过长'    
-	ret,nodeNum,subret,subChr='','','',''    
-	CurChr=['','']     
-	for i in range(5):     
-		j=int(i*4+math.floor(i/4))     
-		subret=''    
-		nodeNum=snum[j:j+4]     
-		lens=len(nodeNum)     
-		for k in range(lens):     
-			if int(nodeNum[k:])==0:     
-				continue    
-			CurChr[k%2] = capNum[int(nodeNum[k:k+1])]     
-			if nodeNum[k:k+1] != '0':     
-				CurChr[k%2] += capDigit[lens][k]     
-			if  not ((CurChr[0]==CurChr[1]) and (CurChr[0]==capNum[0])):     
-				if not((CurChr[k%2] == capNum[0]) and (subret=='') and (ret=='')):     
-					subret += CurChr[k%2]     
-		subChr = [subret,subret+capUnit[i]][subret!='']     
-		if not ((subChr == capNum[0]) and (ret=='')):     
-			ret += subChr     
-		#万亿时没有亿字
-		if len(str(int(num)))>12 and len(str(int(num)))-j<13 and (capUnit[1] not in ret):
-			ret += capUnit[1]
-	#万圆时没有圆字
-	if ret and (not ret.endswith(capUnit[3])):  
-		ret+=capUnit[3]  
-	return [ret,capNum[0]+capUnit[3]][ret=='']  
+import NumToCny
 
 def callback(strs):
 	print('<?xml version="1.0"?>')
@@ -45,4 +12,4 @@ def callback(strs):
 	print('  </item>')
 	print('</items>')
 
-callback(numtoCny({query}))
+callback(NumToCny.to_rmb_upper({query}))
