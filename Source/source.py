f20e9ce9d1737ee119b70e45c39f413d06baf6eb
YourtionGuo <yourtion@gmail.com>
2017-01-18 22:32:02 +0800

    Fixed #7

--- Source/source.py
+++ Source/source.py
@@ -4,8 +4,8 @@ import NumToCny
 import json
 
 def parse(strs):
-    res = {"title": strs,"subtitle": "Copy to clipboard","arg": "Copy to clipboard","icon": "icon.png"}
-    return {"items": [res]}
+    res = { "title": strs, "subtitle": "Copy to clipboard","arg": strs, "icon": "icon.png" }
+    return { "items": [res] }
 
 ret = NumToCny.to_rmb_upper('{query}')
 
